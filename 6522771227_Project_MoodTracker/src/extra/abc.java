17744451bc7a995c9768c68e0906b4ca1535e1d7
Astra3usZ <poonnaphop.su@gmail.com>
2023-04-28 21:39:49 +0700

    Apr 28, 2023

---
+++ 6522771227_Project_MoodTracker/src/extra/abc.java
@@ -0,0 +1,11 @@
+package extra;
+
+public class abc {
+    protected int x;
+    protected long y;
+    
+    public abc() {
+        this.x = 10;
+        this.y = 100L;
+    }
+}
---
+++ 6522771227_Project_MoodTracker/src/extra/xyz.java
@@ -0,0 +1,14 @@
+package extra;
+
+public class xyz extends abc {
+    public xyz() {
+        super();
+        this.x = (int) this.y;
+    }
+    
+    public xyz(int x, long y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+}
